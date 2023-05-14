0c57089283cca89c942bc3e1885eb21018d2d16d
yoannet93 <yoannet93@gmail.com>
2023-05-14 18:36:54 +0200

    fix problems with scrollspy

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -22,7 +22,7 @@ const observer = new IntersectionObserver((entradas, observador)=>{
    })
 }, {
     threshold: 0.1, //para que todo el encabezado entre dentro de pantalla. !!!!!si lo aumento deja de verse la seccion projects.
-    rootMargin: "0px 0px -20% 0px" //establece los margenes del viewport
+    rootMargin: "0px 0px -50% 0px" //establece los margenes del viewport
 });
 
 
