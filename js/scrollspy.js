f97554b5e07276e39275d0ff90c4c9ec9a7ff93e
yoannet93 <yoannet93@gmail.com>
2023-05-14 18:27:51 +0200

    save scrollspy v1

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -1,27 +1,33 @@
-const sections = document.querySelectorAll(".sect");
-const links = document.querySelectorAll("#navbar a")
+const list_sections = document.querySelectorAll(".secciones .sect");
+const links = document.querySelectorAll("#navbar a");
 
 
-const observer = new IntersectionObserver((entry, observador)=>{
-  entry.forEach(entry=>{
-    if(entry.isIntersecting){
-    const id = '#' + entry.target.id;
-    links.forEach(link=>{
-        link.classList.remove('activo')
-        const href = link.attributes.href.nodeValue;
+
+const observer = new IntersectionObserver((entradas, observador)=>{
+   
+    entradas.forEach(item =>{
+    if(item.isIntersecting){
+        const id = '#' + item.target.id;
+        history.pushState({}, item.target.innerText, id); //para modificar la barra de direcciones
+      
+        links.forEach(link_menu =>{
+        const  href = link_menu.attributes.href.nodeValue;
+        link_menu.classList.remove("activo");
         if(href === id){
-          link.classList.add('activo');
+            console.log(link_menu);
+            link_menu.classList.add("activo");
         }
-
-
-    });
+       })
     }
-  })
-},{
-    threshold:1,
-    rootMargin: '0px 0px -50% 0px'
+   })
+}, {
+    threshold: 1, //para que todo el encabezado entre dentro de pantalla.
+    rootMargin: "0px 0px -20% 0px" //establece los margenes del viewport
 });
 
-sections.forEach(item=>{
-    observer.observe(item);
-}); //le digo que observe cada elemento de la lista sections
+
+//con esta instruccion hago que se observe cada seccion destro de la lista de secciones.
+//cada vez que un item entra en pantalla se ejecuta el código.
+list_sections.forEach(item => {
+  observer.observe(item);
+});
